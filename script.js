73d7337521c9023e7c9c25053748c28a4e63191f
sirohiHarsh <sirohiharsh5@gmail.com>
2025-08-20 20:25:31 +0530

    Implement active nav link highlighting on scroll

--- script.js
+++ script.js
@@ -14,3 +14,21 @@
                 loop: true,
             });
         }
+
+const sections = document.querySelectorAll('section[id]');
+const navLinks = document.querySelectorAll('#header nav a');
+    window.addEventListener('scroll', () => {
+        let current = 'home'; 
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+                if (pageYOffset >= sectionTop - 100) {
+                    current = section.getAttribute('id');
+                }
+            });
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                if (link.getAttribute('href').includes(current)) {
+                    link.classList.add('active');
+            }
+        });
+    });
